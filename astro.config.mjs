69889f58e1988d197c744a4a29760bf9d7fa6b1f
githubczl <2088726620@qq.com>
2025-08-04 23:51:29 +0800

    配置 Astro GitHub Pages 自动部署

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,7 +21,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "static",
+	site: "https://githubczl.github.io/myBlog/",
 	base: "/githubczl/myBlog",
 	trailingSlash: "always",
 	integrations: [
